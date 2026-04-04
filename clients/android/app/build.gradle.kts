98109f4995f5238e8a5ee2ba46e1184f578bb726
GraciousGazelles <55840159+GraciousGazelles@users.noreply.github.com>
2026-04-04 22:07:21 +1100

    android: track Rust workspace inputs for JNI build task

--- clients/android/app/build.gradle.kts
+++ clients/android/app/build.gradle.kts
@@ -1,9 +1,13 @@
 import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
+import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
 import org.gradle.process.ExecOperations
 import java.io.ByteArrayOutputStream
@@ -19,6 +23,10 @@ abstract class BuildSolarlabNativeTask : DefaultTask() {
 
     @get:Input
     abstract val ffiCrateRelativePath: Property<String>
+
+    @get:InputFiles
+    @get:PathSensitive(PathSensitivity.RELATIVE)
+    abstract val rustWorkspaceInputs: ConfigurableFileCollection
 
     @get:OutputDirectory
     abstract val generatedJniLibsDir: DirectoryProperty
@@ -169,6 +177,15 @@ val buildSolarlabNative by tasks.registering(BuildSolarlabNativeTask::class) {
     workspaceRootPath.set(workspaceRootDir.absolutePath)
     ffiCrateRelativePath.set("engine/ffi")
     generatedJniLibsDir.set(solarlabGeneratedJniLibsDir)
+    rustWorkspaceInputs.from(
+        fileTree(workspaceRootDir) {
+            include("Cargo.toml")
+            include("Cargo.lock")
+            include("engine/**/Cargo.toml")
+            include("engine/**/src/**/*.rs")
+            include("engine/ffi/include/**/*.h")
+        }
+    )
 }
 
 android {
